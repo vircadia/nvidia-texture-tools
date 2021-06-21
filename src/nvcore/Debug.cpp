7eb846ad2d69d4a32680c60e56de88e413a47dbf
Dale Glass <dale@daleglass.net>
2021-06-22 00:34:00 +0200

    Enable building on aarch64
    According to:
    
    * https://github.com/castano/nvidia-texture-tools/pull/309
    * https://github.com/castano/nvidia-texture-tools/pull/322
    
    Co-authored-by: Julian Groß <julian.g@posteo.de>

--- src/nvcore/Debug.cpp
+++ src/nvcore/Debug.cpp
@@ -1008,6 +1008,13 @@ void debug::dumpInfo()
 #endif
 }
 
+static va_list getEmptyVAList(va_list list, ...)
+{
+    va_start(list, list);
+    va_end(list);
+    return list;
+}
+
 /// Dump callstack using the specified handler.
 void debug::dumpCallstack(MessageHandler *messageHandler, int callstackLevelsToSkip /*= 0*/)
 {
@@ -1020,8 +1027,11 @@ void debug::dumpCallstack(MessageHandler *messageHandler, int callstackLevelsToS
         Array<const char *> lines;
         writeStackTrace(trace, size, callstackLevelsToSkip + 1, lines);     // + 1 to skip the call to dumpCallstack
 
+        va_list empty;
+        empty = getEmptyVAList(empty);
+
         for (uint i = 0; i < lines.count(); i++) {
-            messageHandler->log(lines[i], NULL);
+            messageHandler->log(lines[i], empty);
             delete lines[i];
         }
     }
